d7569ec5a3528acff21b64279dca92a9808cecfc
mutombithomas <mutombithomas@gmail.com>
2023-03-14 20:32:41 +0000

    comments for flask understanding

--- only_flaskapp/app.py
+++ only_flaskapp/app.py
@@ -8,3 +8,10 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+# GitHub Pages does not support custom ports, and all traffic is served over the standard 
+# HTTP and HTTPS ports (80 and 443, respectively). Therefore, you do not need to specify a 
+# port number for your Flask application when deploying it to GitHub Pages.
+
+# By default, Flask runs on http://localhost:5000/, but you should modify the app.run() 
+# line in your "app.py" file to listen on all available network interfaces using 0.0.0.0 as the host address
